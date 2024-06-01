918355417e9ee3744da16c53a0fc296c13bc78f3
Emerson Seiler <seiler.emerson@gmail.com>
2024-06-01 17:51:09 -0300

    Update desafio nginx node

---
+++ desafio-nginx-node/node/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb',
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('João')`
+connection.query(sql)
+
+app.get('/', (req,resp) => {
+    // resp.send('<h1>Full Cycle Rocks1!</h1>')
+    const getPeoples = `SELECT * FROM people`;
+    connection.query(getPeoples, (err, results) => {
+        if (err) throw err;
+
+        let peoplesList = '<ul>';
+        results.forEach(person => {
+            peoplesList += `<li>${person.name}</li>`;
+        });
+        peoplesList += '</ul>';
+
+        resp.send(`
+            <h1>Full Cycle Rocks!</h1>
+            ${peoplesList}
+        `);
+    });
+})
+
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
+})
